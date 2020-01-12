28ce26223174dc415d9575d747c2202546f09b58
Admin <admin@Admins-MacBook-Pro.local>
2020-01-12 13:08:50 -0700

    lastcommit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Assignment Code
-var char = "abcdefghijklmnopqrstuvwxyz";
-var upperLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "0123456789";
 var sym = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 var charNum = document.querySelector("#charNum");
@@ -14,7 +13,6 @@ var copy = document.querySelector("#copy");
 //Event Listener to generate random password
 generate.addEventListener("click", function() {
   var characters = char;
-  upperCharBox.checked ? (characters += charNum) : "";
   numBox.checked ? (characters += num) : "";
   symBox.checked ? (characters += sym) : "";
   yourPw.value = password(charNum.value, characters);
